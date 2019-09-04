b0c25a721442d840a14a843591b9fea4ebf8923c
grondo89 <48697201+grondo89@users.noreply.github.com>
2019-09-04 03:34:06 -0300

    Update CellContainer.jsx

--- gameoflife-react/src/react/containers/CellContainer.jsx
+++ gameoflife-react/src/react/containers/CellContainer.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 
-// este es el componente que renderizado muchas veces conforma el tablero del juego. Recibe como props la clase definida en grid container y un metodo onClick para cambiar su clase de css/"estado"
-
+// este es el componente que renderizado muchas veces conforma el tablero del juego. Recibe como props la clase definida en grid container y un metodo onClick para cambiar el valor de su booleano correspondiente en el grid, y por ende su clase de css
 class CellContainer extends Component {
 	changeCellState = () => {
 		this.props.changeCellState(this.props.x, this.props.y);
